5fb146b6dcda10fce951dc85e6d3e1be7b63b4fb
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-26 12:11:52 -0300

    Implementing Routes (React Router)

--- frontend/src/components/main/App.jsx
+++ frontend/src/components/main/App.jsx
@@ -2,18 +2,19 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
+import { BrowserRouter } from "react-router-dom"
 
+import Routes from './Routes'
 import Logo from '../template/Logo'
 import Nav from '../template/Nav'
-import Home from '../home/Home'
 import Footer from '../template/Footer'
-import Header from '../template/Header'
 
 export default props =>
+<BrowserRouter>
 <div className="app">
     <Logo/>
     <Nav/>
-    <Header/>
-    <Home/>
+    <Routes/>
     <Footer/>
 </div>
+</BrowserRouter>
