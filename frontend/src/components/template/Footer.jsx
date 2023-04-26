5c2b83682615919371a1a53c906bc18c759b72bd
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-26 11:24:33 -0300

    Footer Component

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -2,6 +2,9 @@ import './Footer.css'
 import React from 'react'
 
 export default props =>
-<footer className="footer">
-    Rodapé
-</footer>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong> <span className="text-danger">Ricardo</span></strong>
+        </span>
+    </footer>
